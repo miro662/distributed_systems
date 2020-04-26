05e76e1f5763aba8f4761ee3d181946d3ad12a9a
Mirosław <miro662@gmail.com>
2020-04-26 23:59:26 +0200

    [Z4b] Refactor

--- Blazej_Miroslaw_4/ice/client/main.py
+++ Blazej_Miroslaw_4/ice/client/main.py
@@ -2,7 +2,7 @@ import sys, Ice
 import SmartHouse
 
 
-class BulbulatorClient:
+class Client:
     COMMANDS = {}
 
     @classmethod
@@ -13,12 +13,15 @@ class BulbulatorClient:
     def __init__(self, communicator, name):
         self._name = name
         base = communicator.stringToProxy(f"{name}:default -p 10000")
-        self._stub = SmartHouse.BulbulatorPrx.checkedCast(base)
+        self._stub = self.get_stub(base)
 
     def interact(self):
         while True:
             print(f'{self._name}> ', end='')
-            command_name = input()
+            try:
+                command_name = input()
+            except KeyboardInterrupt:
+                break
             if command_name in self.COMMANDS.keys():
                 self.COMMANDS[command_name](self)
             elif command_name == 'exit':
@@ -27,22 +30,49 @@ class BulbulatorClient:
                 print('Invaild command, use help for commands list')
 
 
-@BulbulatorClient.command
+@Client.command
 def help(self):
     print(f"Available commands: {', '. join(self.COMMANDS)}, exit")
 
 
+class BulbulatorClient(Client):
+    def get_stub(self, base):
+        return SmartHouse.BulbulatorPrx.checkedCast(base)
+
+
 @BulbulatorClient.command
 def mumble(self):
-    self._stub.mumble()
+    print(self._stub.mumble())
 
 
-with Ice.initialize(sys.argv) as communicator:
-    client = BulbulatorClient(communicator, "Bulbulator1")
-    client.interact()
-    base = communicator.stringToProxy("Bulbulator1:default -p 10000")
-    bulbulator = SmartHouse.BulbulatorPrx.checkedCast(base)
-    if not bulbulator:
-        raise RuntimeError("Invaild proxy")
+AVAILABLE_TYPES = {
+    'bulbulator': BulbulatorClient
+}
 
-    bulbulator.mumble()
+if __name__ == '__main__':
+    with Ice.initialize(sys.argv) as communicator:
+        while True:
+            print('> ', end='')
+            try:
+                command = input()
+            except KeyboardInterrupt:
+                break
+
+            split_command = command.rstrip().split(' ')
+            if split_command[0] == 'help':
+                print('syntax: <type> <name>')
+            elif split_command[0] == 'exit':
+                break
+            elif len(split_command) == 2:
+                client_class = AVAILABLE_TYPES.get(split_command[0])
+                if client_class is None:
+                    print(f'Unknown type: {split_command[0]}, available types: {", ".join(AVAILABLE_TYPES.keys())}')
+                    continue
+                
+                try:
+                    client = client_class(communicator, split_command[1])
+                    client.interact()
+                except Ice.ObjectNotExistException:
+                    print(f"Object {split_command[1]} does not exist")
+            else:
+                print(f'Invaild command: {split_command[0]}')
